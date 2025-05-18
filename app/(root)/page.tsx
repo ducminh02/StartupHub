24e7adc0d09ece75b48d8a83814c9a2faeb4c9c6
Minh Vu <d.m.vu@lynxbroker.de>
2025-05-18 12:59:55 +0200

    [ADD] Imporved GitHub authentication with Sanity, create user session management, and implement startup submission form. Update package dependencies and add new UI components for input and toast notifications.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,12 +2,18 @@ import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 import { STARTUP_QUERY } from '@/sanity/lib/queries';
 import { sanityFetch, SanityLive } from '@/sanity/lib/live';
+import { auth } from '@/auth';
 
 export default async function Home({searchParams} : {
 	searchParams: Promise <{query?: string }>
 }) {
 	const query = (await searchParams).query
 	const params = {search: query || null }
+
+	const session = await auth()
+
+	console.log(session?.id)
+	
 	const {data: posts } = await sanityFetch({query: STARTUP_QUERY, params})
 
 	return (
